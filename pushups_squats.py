d32751ef7108ecaac9ed4711eea995daa2d7ef4d
lyfjourney <joshua.farara@gmail.com>
2024-03-26 15:36:46 -0400

    Changes to be committed: 	modified:   pushups_squats.py

--- pushups_squats.py
+++ pushups_squats.py
@@ -1,3 +1,9 @@
+'''
+Calculates pushups where the person does 10 push ups decrementing by 1 to one
+and find the total number of push ups they have done by the end of the workout
+
+'''
+
 total_pushups = 0
 
 for i in range(10, 0, -1):
